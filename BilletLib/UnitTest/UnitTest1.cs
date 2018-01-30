371aa6248d53e717dbb4bd599fa0953386110e42
IanMFrost <ianmfrost0@gmail.com>
2018-01-30 14:28:10 +0100

    Oprettet klasse samt Unit Test

---
+++ BilletLib/UnitTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BilletLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPrisEr240()
+        {
+            // arrange
+             Bil Toyota =new Bil();
+
+            // act
+            int pris = Toyota.Pris();
+            // Assert
+            Assert.AreEqual(240,pris);
+        }
+
+
+        [TestMethod]
+        public void TestForKøretøj()
+        {
+
+        }
+    }
+}
