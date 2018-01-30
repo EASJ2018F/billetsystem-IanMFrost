c8334757a44f54b75e44b66c318dcddb2ae0e2d1
IanMFrost <ianmfrost0@gmail.com>
2018-01-30 14:39:54 +0100

    Oprettet MC klasse samt unit test for klassen

---
+++ BilletLib/UnitTest/UntiTestForMC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BilletLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestForMc
+    {
+        [TestMethod]
+        public void TestMcKoster125()
+        {
+            // arrange
+            MC suzuki = new MC();
+            // act
+            int pris = suzuki.Pris();
+            // assert
+            Assert.AreEqual(125, pris);
+
+        }
+
+        [TestMethod]
+        public void TestOmDenReturnerMC()
+        {
+            // arrange
+            MC Suzuki = new MC();
+            //act
+            string køretøj = Suzuki.køretøj();
+            // Assert
+            Assert.AreEqual("MC", køretøj);
+
+        
+        }
+    }
+}
